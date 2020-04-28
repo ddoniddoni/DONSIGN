048f35e42082a09beb4d882c86d774ccb699487c
ddoniddoni <psdkei@naver.com>
2020-04-28 18:02:39 +0900

    modify css

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const colorWrapper = document.querySelector(".color__wrapper");
 const colorArray = [];
 
 const randomColor = () => {
-  for (let i = 0; i <= 5; i++) {
+  for (let i = 0; i <= 50; i++) {
     let randomNum = Math.floor(Math.random() * 1000000) + 1000000;
     if (randomNum > 1000000) {
       randomNum = randomNum - 1000000;
