f5c378fcdeba4af6dabf0f4e3860db7dddbc2850
ddoniddoni <psdkei@naver.com>
2020-04-29 02:36:33 +0900

    modify css

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const colorWrapper = document.querySelector(".color__wrapper");
-const colorArray = [];
+
+const colorChars = "0123456789";
+const chars_length = 6;
 
 const randomColor = () => {
   for (let i = 0; i <= 50; i++) {
-    let randomNum = Math.floor(Math.random() * 1000000) + 1000000;
-    if (randomNum > 1000000) {
-      randomNum = randomNum - 1000000;
-      colorArray.push(randomNum);
+    let colorArray = "";
+    for (let j = 0; j < chars_length; j++) {
+      let randomNum = Math.floor(Math.random() * chars_length);
+      colorArray += colorChars.substring(randomNum, randomNum + 1);
     }
     const colorCard = document.createElement("div");
     colorCard.classList = "color-card";
-    colorCard.style.backgroundColor = `#${colorArray[i]}`;
-    colorCard.textContent = `color:#${colorArray[i]}`;
+    colorCard.style.backgroundColor = `#${colorArray}`;
+    colorCard.textContent = `color:#${colorArray}`;
     colorWrapper.append(colorCard);
   }
-  console.log(colorArray);
 };
 
 randomColor();
