03d389dfb3ebf8fb8348027e6c3327eafd2cd69c
ddoniddoni <psdkei@naver.com>
2020-04-28 17:52:35 +0900

    add randomColor

---
+++ index.js
@@ -0,0 +1,20 @@
+const colorWrapper = document.querySelector(".color__wrapper");
+const colorArray = [];
+
+const randomColor = () => {
+  for (let i = 0; i <= 5; i++) {
+    let randomNum = Math.floor(Math.random() * 1000000) + 1000000;
+    if (randomNum > 1000000) {
+      randomNum = randomNum - 1000000;
+      colorArray.push(randomNum);
+    }
+    const colorCard = document.createElement("div");
+    colorCard.classList = "color-card";
+    colorCard.style.backgroundColor = `#${colorArray[i]}`;
+    colorCard.textContent = `color:#${colorArray[i]}`;
+    colorWrapper.append(colorCard);
+  }
+  console.log(colorArray);
+};
+
+randomColor();
