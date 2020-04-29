0ff2015dbd2e0376f0125c3e52553c49e17f5dda
ddoniddoni <psdkei@naver.com>
2020-04-29 18:21:37 +0900

    change div -> button

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const colorWrapper = document.querySelector(".color__wrapper");
-const colorCard = document.createElement("div");
 const colorChars = "0123456789";
 const chars_length = 6;
+
 const randomColor = () => {
-  for (let i = 0; i <= 50; i++) {
+  for (let i = 0; i <= 100; i++) {
     let colorArray = "";
 
     for (let j = 0; j < chars_length; j++) {
       let randomNum = Math.floor(Math.random() * 10);
       colorArray += colorChars.substring(randomNum, randomNum + 1);
     }
-    const colorCard = document.createElement("img");
-    colorCard.classList = "color-card";
-    colorCard.style.backgroundColor = `#${colorArray}`;
-    colorCard.textContent = `color:#${colorArray}`;
-    colorWrapper.append(colorCard);
+    const colorImg = document.createElement("button");
+    colorImg.classList = "color-card";
+    colorImg.style.backgroundColor = `#${colorArray}`;
+    colorImg.textContent = `color:#${colorArray}`;
+    colorWrapper.append(colorImg);
   }
 };
 
