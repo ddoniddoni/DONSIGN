67e9b4a14a24e97f486154c144300dc441f78e5d
ddoniddoni <psdkei@naver.com>
2020-04-29 18:02:45 +0900

    add randomNum

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const colorWrapper = document.querySelector(".color__wrapper");
-
+const colorCard = document.createElement("div");
 const colorChars = "0123456789";
 const chars_length = 6;
-
 const randomColor = () => {
   for (let i = 0; i <= 50; i++) {
     let colorArray = "";
+
     for (let j = 0; j < chars_length; j++) {
-      let randomNum = Math.floor(Math.random() * chars_length);
+      let randomNum = Math.floor(Math.random() * 10);
       colorArray += colorChars.substring(randomNum, randomNum + 1);
     }
-    const colorCard = document.createElement("div");
+    const colorCard = document.createElement("img");
     colorCard.classList = "color-card";
     colorCard.style.backgroundColor = `#${colorArray}`;
     colorCard.textContent = `color:#${colorArray}`;
