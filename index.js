fcc00e6b10c4e456be21250296b472b7af17a80c
ddoniddoni <psdkei@naver.com>
2020-06-18 16:09:23 +0900

    Change color

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const colorWrapper = document.querySelector(".color__wrapper");
-const colorChars = "0123456789";
+const colorChars = "0123456789ABCDEF";
 const chars_length = 6;
 
 const randomColor = () => {
-  for (let i = 0; i <= 100; i++) {
+  for (let i = 0; i <= 104; i++) {
     let colorArray = "";
 
     for (let j = 0; j < chars_length; j++) {
-      let randomNum = Math.floor(Math.random() * 10);
+      let randomNum = Math.floor(Math.random() * 16);
       colorArray += colorChars.substring(randomNum, randomNum + 1);
     }
     const colorImg = document.createElement("button");
