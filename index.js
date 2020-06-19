d3f894ff93af117e710871ad8f20a942a7804892
ddoniddoni <psdkei@naver.com>
2020-06-19 17:50:44 +0900

    Update Express Setting

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello, World!");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on : https://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening); // 4000번 포트로!
