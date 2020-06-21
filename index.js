7b325da2606d283d5e362b2c89feb4f6cc70531d
ddoniddoni <psdkei@naver.com>
2020-06-21 15:19:12 +0900

    Handling Routes with Express

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ function handleListening() {
   console.log(`Listening on : https://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+  res.send("Hello from home");
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening); // 4000번 포트로!
